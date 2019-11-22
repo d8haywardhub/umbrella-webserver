b0563a973b83b892c725539bdff582394384aebf
d8haywardhub <d8hayward@gmail.com>
2019-11-22 09:59:13 -0500

    Docker

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'behavior'`, () => {
+  it(`should have as title 'umbrella-webserver'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('behavior');
+    expect(app.title).toEqual('umbrella-webserver');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to behavior!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to umbrella-webserver!');
   });
 });
